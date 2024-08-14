513179c50ed3deeed3247b6b864aba497c0b0c2c
Karel van Zijl <karelvanzijl@Karels-MacBook-Pro.local>
2024-08-14 19:12:05 +0800

    [CHORE] add latest files for 2024-08-14

---
+++ Stage 2/Classes/2024-08-14/node/logger/app.js
@@ -0,0 +1,12 @@
+// Import the logger module
+const logger = require("./logger.js");
+
+// Log an info message
+logger.info("Info message");
+
+// Log an error message
+logger.error("Error message");
+
+// Try:
+// console.log(loggerModule);
+// console.log(loggerModule.info);
