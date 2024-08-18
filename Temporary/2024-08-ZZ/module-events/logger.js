46a19fc6dc12a2200865428280bdb1b600eaac97
Karel van Zijl <karelvanzijl@Karels-MacBook-Pro.local>
2024-08-18 16:15:43 +0800

    [CHORE] prep for event module class

---
+++ Temporary/2024-08-ZZ/module-events/logger.js
@@ -0,0 +1,15 @@
+const EventEmitter = require("events");
+
+var url = "https://someloggerservice.io";
+
+class Logger extends EventEmitter {
+    log(message) {
+        // Send HTTP request
+        console.log(message);
+
+        // Raise an event
+        this.emit("messageLogged", { id: 1, message: "db error message" });
+    }
+}
+
+module.exports = Logger;
