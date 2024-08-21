2ebb7bfeda255eb66ce0a60c83ca862a2d2d5b73
Karel van Zijl <karelvanzijl@Karels-MacBook-Pro.local>
2024-08-21 16:06:00 +0800

    [CHORE] remove some files from prep module http folder. These files have been copied to 2024-08-22 class section

--- Temporary/2024-08-ZZ/module-http/index.js
+++ Temporary/2024-08-ZZ/module-http/index.js
@@ -7,10 +7,13 @@ const server = http.createServer((request, response) => {
     }
 
     if (request.url === "/api/products") {
-        // Database query that gets all the products
-        // SELECT * FROM products;
-        // Lets assume our mysql2 package return the following object as teh result:
+        /*
+            Database query that gets all the products
 
+            SELECT * FROM products;
+            
+            Lets assume our mysql2 package return the following object as the query result
+        */
         const result = [
             {
                 id: 1,
@@ -26,11 +29,11 @@ const server = http.createServer((request, response) => {
             },
         ];
 
-        // Now we're going to send back this result using a JSON string:
-        // Memember:
-        //
-        // JSON.stringify()
-
+        /*
+            Now we're going to send back this result using a JSON string
+            
+            Remember: JSON.stringify()
+        */
         const response = JSON.stringify(result);
 
         response.write(response);
